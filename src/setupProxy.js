8f26ef68f1707f35e6ccd466dca6a8df11564b74
Afonso Camacho <96800008+afonsoocamacho@users.noreply.github.com>
2024-08-19 17:15:03 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8000",
+      target: "https://introweek-runcmd-website-e0032d4f624f.herokuapp.com",
       changeOrigin: true,
     })
   );
